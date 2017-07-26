7d4014a329dbf916f09f6440e391643070997b7b
minje.choi <minje.choi@navercorp.com>
2017-07-26 10:51:13 +0900

    Moved functions to subdirectory

---
+++ packages/functions.py
@@ -0,0 +1,27 @@
+import torch
+from torch.autograd import Variable
+
+def to_var(x):
+    if torch.cuda.is_available():
+        x = x.cuda()
+    return Variable(x)
+
+def reset_grad(model):
+    model.zero_grad()
+    
+def extract(text): # extracts model structure from source code
+    import re
+    text = text.split('\n')
+    numbers = []
+    activation = []
+    for line in text:
+        if 'Leaky' not in line:
+            numbers.extend(re.findall(r'\d+',line))
+        if ('LU' in line):
+            activation.append(line.split('(')[0].strip())
+    numbers = [x for i,x in enumerate(numbers) if i%2==0]
+    activation = list(set(activation))[0]
+    result = line.split('(')[0].strip()
+    if 'Linear' in result:
+        result = None
+    return numbers, activation, result
